fc92152c266618d236fbcb0603d4fef78d024609
biomiller <92millero@gmail.com>
2019-06-04 14:48:33 +0100

    added map implementation

--- src/helloworld/App.java
+++ src/helloworld/App.java
@@ -1,9 +1,24 @@
 package helloworld;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
+	
+	Map<String,String> appMap = new HashMap<String,String>();
 	
 	public void hello() {
 		System.out.println("Hello world to the standard out.");
 	}
+	
+	
+	public void newEntry(String name, String job) {
+		this.appMap.put(name, job);
+	}
+	
+	public String getEntry(String name) {
+		return this.appMap.get(name);
+	}
+	
 
 }
