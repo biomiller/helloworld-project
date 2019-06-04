321cd0272eead671a50918fd658e2b6d4cb412ec
biomiller <92millero@gmail.com>
2019-06-04 14:40:17 +0100

    first commit

---
+++ src/helloworld/App.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class App {
+	
+	public void hello() {
+		System.out.println("Hello world to the standard out.");
+	}
+
+}
---
+++ src/helloworld/Runner.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		
+		App app1 = new App();
+		
+		app1.hello();
+	}
+
+}
